b50c771a3b456b72f27b166828b1346c151711c4
fifty <mohamedelameen50>
2021-11-25 17:34:57 +0400

    remove app bar from web page

--- CustomerPulseSurvey/src/main/java/com/customerpulse/customerpulsesurvey/view/WebViewActivity.java
+++ CustomerPulseSurvey/src/main/java/com/customerpulse/customerpulsesurvey/view/WebViewActivity.java
@@ -1,7 +1,5 @@
 package com.customerpulse.customerpulsesurvey.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
